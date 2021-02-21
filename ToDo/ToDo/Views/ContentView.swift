e5138ff9e3e3897cf633f35aab0058a9424b4ab7
Patryk Leszner <patryk.leszner@gmail.com>
2021-02-21 20:31:30 +0100

    ToDoApp
    Changes:
    -Some color added,
    -Made code cleaner

--- ToDo/ToDo/Views/ContentView.swift
+++ ToDo/ToDo/Views/ContentView.swift
@@ -10,73 +10,62 @@ import SwiftUI
 struct ContentView: View {
     @ObservedObject var TaskListVM = TaskListViewModel()
     let tasks = testDataTasks
-
     @State var presentAddNewItem = false
     @State private var editMode = EditMode.inactive
-    
     var body: some View {
-     
-        VStack {
-            NavigationView {
-                VStack(alignment: .leading) {
-                    List{
-                    ForEach(TaskListVM.taskCellViewModels) { taskCellVM in
-                        TaskCell(taskCellVM: taskCellVM)
-                    }
-                    .onDelete(perform: onDelete)
-                    .onMove(perform: onMove)
-                         
-                        if presentAddNewItem{
-                            TaskCell(taskCellVM: TaskCellViewModel(task: Task(title: "", completed: false))) { task in
-                                self.TaskListVM.addTask(task: task)
-                                self.presentAddNewItem.toggle()
-                                    
+            VStack {
+                NavigationView {
+                    ZStack{
+                        LinearGradient(gradient: Gradient(colors: [Color.white, Color.purple]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                            .edgesIgnoringSafeArea(.all)
+                        VStack(alignment: .leading){
+                            List{
+                                ForEach(TaskListVM.taskCellViewModels) { taskCellVM in
+                                    TaskCell(taskCellVM: taskCellVM)
+                                }
+                                .onDelete(perform: onDelete)
+                                .onMove(perform: onMove)
+                                if presentAddNewItem{
+                                    TaskCell(taskCellVM: TaskCellViewModel(task: Task(title: "", completed: false))) { task in
+                                        self.TaskListVM.addTask(task: task)
+                                        self.presentAddNewItem.toggle()
+                                    }
+                                }
                             }
-                            
-                            
+                            Button(action: {self.presentAddNewItem.toggle()}) {
+                                HStack{
+                                    Image(systemName: "plus.circle.fill")
+                                        .resizable()
+                                        .frame(width: 30, height: 30)
+                                        .foregroundColor(.white)
+                                    Text("Add New ToDo") .foregroundColor(Color.white)
+                                }
+                            }
+                            .padding()
                         }
-                        
-                        
-                       
-                    }
-                    Button(action: {self.presentAddNewItem.toggle()}) {
-                    HStack{
-                        Image(systemName: "plus.circle.fill")
-                            .resizable()
-                            .frame(width: 30, height: 30)
-                        Text("Add New ToDo")
+                        .navigationBarTitle(Text("To Do"))
+                        .foregroundColor(.purple)
+                        .navigationBarItems(leading: EditButton())
+                        .foregroundColor(.purple)
+                        .environment(\.editMode, $editMode)
                     }
                 }
-                .padding()
-            }
-            .navigationBarTitle("To Do")
-            .navigationBarItems(leading: EditButton())
-            .environment(\.editMode, $editMode)
-        
             }
         }
-
+        private func onDelete(offsets: IndexSet) {
+            TaskListVM.taskCellViewModels.remove(atOffsets: offsets)
+            }
+        private func onMove(source: IndexSet, destination: Int) {
+            TaskListVM.taskCellViewModels.move(fromOffsets: source, toOffset: destination)
+            }
     }
-    private func onDelete(offsets: IndexSet) {
-        TaskListVM.taskCellViewModels.remove(atOffsets: offsets)
-        }
-    private func onMove(source: IndexSet, destination: Int) {
-        TaskListVM.taskCellViewModels.move(fromOffsets: source, toOffset: destination)
-}
-}
-
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            
-            
     }
 }
-
 struct TaskCell: View {
     @ObservedObject var taskCellVM: TaskCellViewModel
-    
     var onCommit: (Task) -> (Void) = { _ in}
     var body: some View {
         HStack{
@@ -85,7 +74,6 @@ struct TaskCell: View {
                 .frame(width: 25, height: 25)
                 .onTapGesture{
                     self.taskCellVM.task.completed.toggle()
-                    
                 }
             TextField("What You need ToDo?", text: $taskCellVM.task.title, onCommit: {
                 self.onCommit(self.taskCellVM.task)
